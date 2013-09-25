1f8e1538bc668969ea6b06f7b0f18eee37b4000b
L Lumbreras <llumbreras@gmail.com>
2013-09-24 19:52:18 -0700

    Setup files commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'sqlite3'
+gem 'high_voltage'
+
+gem 'sass-rails', '~> 3.2.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 3.2.1'
+
+group :test, :development do
+    gem 'rspec-rails'
+    gem 'capybara', '~> 1.1.4'
+end
+
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.0.1'
+
+group :doc do
+    # bundle exec rake doc:rails generates the API under doc/api.
+   gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoer::Application.config.secret_token = '17735498262b24eac4c6b7469b993a6ba8b22ef163a453c066ece2cc3d674814db6bbfebe4229090dcd8ca3bba3c570a9f975c065921b3caf72ee313762a8842'
