65200e70c4b3b34742407c3d2d95851d3a11b296
L Lumbreras <llumbreras@gmail.com>
2013-09-30 21:26:55 -0700

    Modify spec to use the SignInHelpers

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 feature 'Manage todos' do 
 	scenario 'Create a todo' do
-		visit root_path
-
-		fill_in 'Email address', with: 'person@example.com'
-		click_button 'Sign In'
-
+		sign_in
 		click_link 'Add todo'
 		fill_in 'Description', with: 'Buy a book'
 		click_button 'Create todo'
