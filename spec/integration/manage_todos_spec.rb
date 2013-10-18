e7826707d97de016c03771a123a15a2bb22ab307
L Lumbreras <llumbreras@gmail.com>
2013-10-17 20:11:10 -0700

    Add test to view only owned todo

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -9,4 +9,19 @@ feature 'Manage todos' do
 
 		expect(page).to have_css 'li.todo', text: 'Buy a book'
 	end
+
+	scenario 'View only my todo' do
+		Todo.create(description:'Buy a pen', owner_email:'not_me@example.com')
+
+		sign_in_as 'me@example.com'
+
+		click_link "Add todo"
+		fill_in 'Description', with: 'Buy a book'
+		click_button 'Create todo'
+
+		expect(page).to have_css 'li.todo', text: 'Buy a book'
+		expect(page).not_to have_css 'li.todo', text: 'Buy a pen'
+	end
+
+
 end
